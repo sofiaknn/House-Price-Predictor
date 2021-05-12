595a470beaf096a6fbdd53156a36f0474d5a3fa8
sofiaknn <80714983+sofiaknn@users.noreply.github.com>
2021-05-12 19:59:54 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from PIL import Image
+
+st.title("Asunnon hinta-arvio")
+
+st.write("""
+### Tampere, Suomi
+""")
+
+data = pd.read_csv("cleaned_data.csv")
+alldata = pd.read_csv("all_data.csv")
+df = alldata[0:0]
+df = df.drop(['Hinta'], axis=1)
+
+s = st.sidebar.text_input("Neliöt", 0)
+y = st.sidebar.text_input("Rakennusvuosi", 0)
+c_list = data['Kaupunginosa'].tolist()
+options_city = list(dict.fromkeys(c_list))
+
+area_name = st.sidebar.selectbox("Kaupunginosa", options_city)
+
+t_list = data['Tyyppi'].tolist()
+options_type = list(dict.fromkeys(t_list))
+
+type_name = st.sidebar.selectbox("Asunnon tyyppi", options_type)
+
+new_row = {'Neliöt': s, 'Rakennusvuosi': y, area_name: 1, type_name: 1}
+
+df = df.append(new_row, ignore_index=True)
+df = df.fillna(0)
+
+image = Image.open('keskustori_ilmasta.jpg')
+st.image(image)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    alldata.drop(['Hinta'], axis=1),
+    alldata.Hinta, test_size=0.2, random_state=20)
+
+
+def model_pipeline(model, param_grid, scoring):
+    Tuned_Model = GridSearchCV(estimator=model, param_grid=param_grid,
+                               scoring=scoring, cv=3)
+
+    Tuned_Model.fit(X_train, y_train)
+    return Tuned_Model
+
+param_grid = {
+    "n_estimators": [500],
+    "max_features": [0.1],
+}
+model2 = RandomForestRegressor(n_jobs=-1, random_state=0, bootstrap=True)
+Tuned_Model2 = model_pipeline(model2, param_grid, "neg_median_absolute_error")
+best2 = Tuned_Model2.best_estimator_
+
+y_pred = Tuned_Model2.predict(df)
+hinta = int(y_pred)
+hinta = str(hinta)
+s = 'Neliöitä: ' + s + 'm^2' + ', Rakennusvuosi: ' + y + ', Kaupunginosa: ' + area_name + ', Asunnon tyyppi: ' + type_name
+
+
+h_ylaraja = int(hinta)*1.1
+h_alaraja = int(hinta)*0.9
+h_ylaraja = int(h_ylaraja)
+h_alaraja = int(h_alaraja)
+h_ylaraja = str(h_ylaraja)
+h_alaraja = str(h_alaraja)
+arvio = 'Asunnon hinta sijoittuu todennäköisesti välille ' + h_alaraja + ' - ' + h_ylaraja + ' €'
+
+if st.sidebar.button('Laske hinta-arvio'):
+    h = str("Asuntosi arvioitu hinta on noin " + hinta + " €")
+    st.subheader(h)
+    arvio
+
+
